f2ff5011b1207c9f0aa71a86c599c6d42a3a68d3
jinha <jinha4ever@gmail.com>
2022-03-27 12:42:58 +0900

    Spring Security Custom Login Form 적용

--- spring-security-demo2/src/main/java/com/jjh/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo2/src/main/java/com/jjh/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.jjh.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,6 +22,18 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("jinha").password("test123").roles("EMPLOYEE"))
 			.withUser(users.username("sooyeon").password("test123").roles("MANAGER"))
 			.withUser(users.username("yerin").password("test123").roles("ADMIN"));
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {//application, login, logout의 webpaths의 security 설정
+		http.authorizeRequests()
+				.anyRequest().authenticated() //해당 앱으로의 어떤 리퀘스트든 authenticated 되어야 함
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage") //커스텀 로그인 페이지 보여주는 리퀘스트 맵핑
+				.loginProcessingUrl("/authenticateTheUser") //로그인 폼의 POST 데이터 프로세싱할 URL
+				.permitAll();// 로그인 할 필요 없이 모든 사람이 로그인 페이지에 접근할 수 있음.
+		
 	}
 	
 
