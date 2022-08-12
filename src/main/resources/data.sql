fc8be97cdf1a5c32a228c6cc07ffc46491453c49
jinha <77036027+jinhajeong527@users.noreply.github.com>
2022-08-12 14:35:24 +0900

    Delete src directory

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tutorials (id, title, description, published) VALUES (1, 'testTitle1','testDescription1', true);
-INSERT INTO tutorials (id, title, description, published) VALUES (2, 'testTitle2','testDescription2', false);
-INSERT INTO tutorials (id, title, description, published) VALUES (3, 'testTitle3','testDescription3', true);
-INSERT INTO tutorials (id, title, description, published) VALUES (4, 'testTitle4','testDescription4', false);
-INSERT INTO tutorials (id, title, description, published) VALUES (5, 'testTitle5','testDescription5', true);
